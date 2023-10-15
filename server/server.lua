0312d2027e912bc76e1c808dde077556f143cc41
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-15 22:19:07 +0100

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,26 @@
+Requests = {}
+
+lib.callback.register('alv_lawyer:sendRequest', function(source, data)
+    Data = data
+    Data[#Data+1] = {label='Request ID', description=#Requests+1, args={id=#Requests+1}}
+    Requests[#Requests+1] = data
+    return true
+end)
+
+lib.callback.register('alv_lawyer:job', function(source, type, data)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if type == 'fetch' then
+        return Requests
+    elseif type == 'fetchById' then
+        for k, v in pairs(Requests) do
+            if k == data then
+                return v
+            end
+        end
+    elseif type == 'close' then
+
+    elseif type == 'check' then
+        if Config.LawyerJobs[xPlayer.getJob().name] then return true else return false end
+    end
+end)
