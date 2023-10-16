e42f1d6c9806bbf092c26ab43ca190a2a986e305
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-16 13:17:10 +0100

    Create discord.lua

---
+++ discord.lua
@@ -0,0 +1,10 @@
+Discord = {}
+
+Discord.Color = 16711680,
+Discord.Username = 'Alv.gg'
+Discord.AvatarUrl = 'https://avatars.githubusercontent.com/u/72032190?v=4'
+
+Discord.Webhooks = {
+  ['Lawyer Called'] = 'WEBHOOK HERE',
+  ['Request Closed'] = 'WEBHOOK HERE',
+}
